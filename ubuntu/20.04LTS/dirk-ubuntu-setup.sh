4d9c13f98e89dd6fb59a398d5c7b70b27ef65767
Dirkadin <dirkadin@gmail.com>
2020-06-13 19:41:51 -0500

    added docker format and remove intellij

--- ubuntu/20.04LTS/dirk-ubuntu-setup.sh
+++ ubuntu/20.04LTS/dirk-ubuntu-setup.sh
@@ -3,7 +3,8 @@ sudo apt update -y
 sudo apt install docker.io -y
 sudo systemctl enable --now docker
 sudo groupadd docker
-newgrp docker
 sudo usermod -aG docker $USER
-sudo snap install intellij-idea-ultimate --classic
-
+newgrp docker
+export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n" >> .bashrc
+alias dps='docker ps --format="$FORMAT"' >> .bashrc
+source ~/.bashrc
