93b81da259581acc5ea25cb098c549ea27669f1b
Dirkadin <dirkadin@gmail.com>
2021-02-28 20:36:04 -0600

    updated to zsh and fixed paths to .zshrc

--- ubuntu/20.04LTS/dirk-ubuntu-setup.sh
+++ ubuntu/20.04LTS/dirk-ubuntu-setup.sh
@@ -5,6 +5,6 @@ sudo systemctl enable --now docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
-export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n" >> .bashrc
-alias dps='docker ps --format="$FORMAT"' >> .bashrc
-source ~/.bashrc
+export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n" >> .zshrc
+alias dps='docker ps --format="$FORMAT"' >> ~/.zshrc
+source ~/.zshrc
