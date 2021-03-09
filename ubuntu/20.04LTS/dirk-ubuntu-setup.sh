cd8a45563b4006063de079194268910c280cb64b
Dirkadin <dirkadin@gmail.com>
2021-03-08 19:55:33 -0600

    added ohmyzsh

--- ubuntu/20.04LTS/dirk-ubuntu-setup.sh
+++ ubuntu/20.04LTS/dirk-ubuntu-setup.sh
@@ -6,5 +6,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
 export FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{{.Ports}}\nCOMMAND\t{{.Command}}\nCREATED\t{{.CreatedAt}}\nSTATUS\t{{.Status}}\n" >> .zshrc
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+set ZSH_THEME="rkj-repos"
 alias dps='docker ps --format="$FORMAT"' >> ~/.zshrc
 source ~/.zshrc
